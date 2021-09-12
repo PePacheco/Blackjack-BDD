145c3f876a428fd80cad77a4930796ac5e6f7dd7
PePacheco <pedro_pacheco98@outlook.com>
2021-09-12 19:58:56 -0300

    first commit

---
+++ Blackjack.py
@@ -0,0 +1,42 @@
+import random
+
+cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+def nextCard():
+	return random.choice(cards)
+
+def handTotal(hand):
+		values = [None, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10]
+		value_map = {k: v for k, v in zip(cards, values)}
+
+		total = sum([value_map[card] for card in hand if card != 'A'])
+		ace_count = hand.count('A')
+
+		for i in range(ace_count, -1, -1):
+			if i == 0:
+				total = total + ace_count
+			elif total + (i * 11) + (ace_count - i) <= 21:
+				total = total + (i * 11) + ace_count - i
+				break
+
+		return total
+
+
+class Dealer():
+	def __init__(self):
+		self.hand = []
+
+	def newRound(self):
+		self.hand = [nextCard(), nextCard()]
+
+	def getHandTotal(self):
+		return handTotal(self.hand)
+
+	def determinePlay(self, total):
+		if total < 17:
+			return 'hit'
+		else:
+			return 'stand'
+
+	def makePlay(self):
+		return self.determinePlay(self.getHandTotal())
---
+++ features/steps/steps.py
@@ -0,0 +1,67 @@
+from behave import *
+from Blackjack import *
+
+################
+### GIVEN
+################
+
+# First scenario
+@given('a dealer')
+def step_impl(context):
+    context.dealer = Dealer()
+
+# Second scenario
+@given('a hand {total:d}')
+def step_impl(context, total):
+	context.dealer = Dealer()
+	context.total = total
+
+# Third scenario
+@given('a {hand}')
+def step_impl(context, hand):
+	context.dealer = Dealer()
+	context.dealer.hand = hand.split(',')
+
+
+################
+### WHEN
+################
+
+# First scenario
+@when('the round starts')
+def step_impl(context):
+	context.dealer.newRound()
+
+# Second scenario	
+@when('the dealer sums the cards')
+def step_impl(context):
+	context.dealer_total = context.dealer.getHandTotal()
+
+# Third scenario
+@when('the dealer determines a play')
+def step_impl(context):
+	context.dealer_play = context.dealer.determinePlay(context.total)
+
+
+################
+### THEN
+################
+
+# First scenario
+@then('the dealer gives itself two cards')
+def step_impl(context):
+	assert (len(context.dealer.hand) == 2)
+
+@then('the dealer chooses a play')
+def step_impl(context):
+	assert (context.dealer.makePlay() in ['stand', 'hit'])
+
+# Second scenario	
+@then('the {total:d} is correct')
+def step_impl(context, total):
+	assert (context.dealer_total == total)
+
+# Third scenario
+@then('the {play} is correct')
+def step_impl(context, play):
+	assert (context.dealer_play == play)
